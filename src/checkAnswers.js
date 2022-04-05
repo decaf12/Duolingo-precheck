0a663472c3c8133d09f23b490d609f2c33aebef6
decaf <siyang.he.uw@gmail.com>
2022-04-05 10:09:56 -0400

    empty string detection

--- src/checkAnswers.js
+++ src/checkAnswers.js
@@ -28,16 +28,22 @@ export function gradeTranslation(answer, vertices) {
     }
 
     const currToken = answerSplit[currTokenID];
+    console.log(`Current token: ${currToken}`);
 
     vertices[currVertexID].forEach((vertex) => {
-      console.log(`Candidate vertex ID: ${vertex.to}, vertex token: ${vertex.lenient} token length: ${vertex.lenient.length}`);
+      console.log(`Candidate vertex ID: ${vertex.to}, vertex token: ${vertex.lenient} token length: ${vertex.lenient.trim().length}`);
       console.log(`Current visited object: ${JSON.stringify(currVisited)}`);
       if (!(vertex.to in currVisited)) {
+        console.log(`Free vertex: ${vertex.to}`);
+        console.log(`Free vertex trimmed length: ${vertex.lenient.trim().length}`);
         const nextVisited = { ...currVisited };
         nextVisited[vertex.to] = null;
 
-        const lenient = vertex.lenient.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
-        if (lenient === currToken) {
+        if (!vertex.lenient.trim().length) {
+          console.log('Lenient empty');
+          stack.push([vertex.to, currTokenID, { ...nextVisited }]);
+          console.log(`Vertex pushed: ${vertex.to}`);
+        } else if (vertex.lenient === currToken) {
           stack.push([vertex.to, currTokenID + 1, { ...nextVisited }]);
           console.log(`Vertex pushed: ${vertex.to}`);
         } else if ('orig' in vertex) {
@@ -46,9 +52,6 @@ export function gradeTranslation(answer, vertices) {
             stack.push([vertex.to, currTokenID + 1, { ...nextVisited }]);
             console.log(`Vertex pushed: ${vertex.to}`);
           }
-        } else if (lenient === '' || lenient === ' ') {
-          stack.push([vertex.to, currTokenID, { ...nextVisited }]);
-          console.log(`Vertex pushed: ${vertex.to}`);
         }
       }
     });
