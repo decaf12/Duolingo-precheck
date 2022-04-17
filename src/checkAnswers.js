7d69c4e609c7290e0d938f438df1bab053aca221
decaf <siyang.he.uw@gmail.com>
2022-04-16 20:33:58 -0400

    lenient with accent marks

--- src/checkAnswers.js
+++ src/checkAnswers.js
@@ -1,8 +1,35 @@
 import { TSMap } from 'typescript-map';
 import * as constants from './challengeTypeConstants';
 
-function caseInsensitiveCmp(string1, string2) {
-  return string1.toLowerCase() === string2.toLowerCase();
+const accentMap = JSON.parse('{"À":"A","Á":"A","Â":"A","Ã":"A","Ä":"A","Å":"A","Ç":"C","È":"E","É":"E","Ê":"E","Ë":"E","Ì":"I","Í":"I","Î":"I","Ï":"I","Ð":"D","Ñ":"N","Ò":"O","Ó":"O","Ô":"O","Õ":"O","Ö":"O","×":"x","Ø":"O","Ù":"U","Ú":"U","Û":"U","Ü":"U","Ý":"Y","à":"a","á":"a","â":"a","ã":"a","ä":"a","å":"a","ç":"c","è":"e","é":"e","ê":"e","ë":"e","ì":"i","í":"i","î":"i","ï":"i","ð":"d","ñ":"n","ò":"o","ó":"o","ô":"o","õ":"o","ö":"o","÷":"/","ø":"o","ù":"u","ú":"u","û":"u","ü":"u","ý":"y","ÿ":"y","Ā":"A","ā":"a","Ă":"A","ă":"a","Ą":"A","ą":"a","Ć":"C","ć":"c","Ĉ":"C","ĉ":"c","Ċ":"C","ċ":"c","Č":"C","č":"c","Ď":"D","ď":"d","Đ":"D","đ":"d","Ē":"E","ē":"e","Ĕ":"E","ĕ":"e","Ė":"E","ė":"e","Ę":"E","ę":"e","Ě":"E","ě":"e","Ĝ":"G","ĝ":"g","Ğ":"G","ğ":"g","Ġ":"G","ġ":"g","Ģ":"G","ģ":"g","Ĥ":"H","ĥ":"h","Ħ":"H","ħ":"h","Ĩ":"I","ĩ":"i","Ī":"I","ī":"i","Ĭ":"I","ĭ":"i","Į":"I","į":"i","İ":"I","ı":"i","Ĵ":"J","ĵ":"j","Ķ":"K","ķ":"k","ĸ":"k","Ĺ":"L","ĺ":"l","Ļ":"L","ļ":"l","Ľ":"L","ľ":"l","Ŀ":"L","ŀ":"l","Ł":"L","ł":"l","Ń":"N","ń":"n","Ņ":"N","ņ":"n","Ň":"N","ň":"n","Ō":"O","ō":"o","Ŏ":"O","ŏ":"o","Ő":"O","ő":"o","Ŕ":"R","ŕ":"r","Ŗ":"R","ŗ":"r","Ř":"R","ř":"r","Ś":"S","ś":"s","Ŝ":"S","ŝ":"s","Ş":"S","ş":"s","Š":"S","š":"s","Ţ":"T","ţ":"t","Ť":"T","ť":"t","Ŧ":"T","ŧ":"t","Ũ":"U","ũ":"u","Ū":"U","ū":"u","Ŭ":"U","ŭ":"u","Ů":"U","ů":"u","Ű":"U","ű":"u","Ų":"U","ų":"u","Ŵ":"W","ŵ":"w","Ŷ":"Y","ŷ":"y","Ÿ":"Y","Ź":"Z","ź":"z","Ż":"Z","ż":"z","Ž":"Z","ž":"z","Ơ":"O","ơ":"o","Ư":"U","ư":"u","Ș":"S","ș":"s","Ț":"T","ț":"t","Ά":"Α","Έ":"Ε","Ή":"Η","Ί":"Ι","Ό":"Ο","Ύ":"Υ","Ώ":"Ω","ΐ":"ι","Ϊ":"Ι","Ϋ":"Υ","ά":"α","έ":"ε","ή":"η","ί":"ι","ΰ":"υ","ϊ":"ι","ϋ":"υ","ό":"ο","ύ":"υ","ώ":"ω","Ạ":"A","ạ":"a","Ả":"A","ả":"a","Ấ":"A","ấ":"a","Ầ":"A","ầ":"a","Ẩ":"A","ẩ":"a","Ẫ":"A","ẫ":"a","Ậ":"A","ậ":"a","Ắ":"A","ắ":"a","Ằ":"A","ằ":"a","Ẳ":"A","ẳ":"a","Ẵ":"A","ẵ":"a","Ặ":"A","ặ":"a","Ẹ":"E","ẹ":"e","Ẻ":"E","ẻ":"e","Ẽ":"E","ẽ":"e","Ế":"E","ế":"e","Ề":"E","ề":"e","Ể":"E","ể":"e","Ễ":"E","ễ":"e","Ệ":"E","ệ":"e","Ỉ":"I","ỉ":"i","Ị":"I","ị":"i","Ọ":"O","ọ":"o","Ỏ":"O","ỏ":"o","Ố":"O","ố":"o","Ồ":"O","ồ":"o","Ổ":"O","ổ":"o","Ỗ":"O","ỗ":"o","Ộ":"O","ộ":"o","Ớ":"O","ớ":"o","Ờ":"O","ờ":"o","Ở":"O","ở":"o","Ỡ":"O","ỡ":"o","Ợ":"O","ợ":"o","Ụ":"U","ụ":"u","Ủ":"U","ủ":"u","Ứ":"U","ứ":"u","Ừ":"U","ừ":"u","Ử":"U","ử":"u","Ữ":"U","ữ":"u","Ự":"U","ự":"u","Ỳ":"Y","ỳ":"y","Ỵ":"Y","ỵ":"y","Ỷ":"Y","ỷ":"y","Ỹ":"Y","ỹ":"y"}');
+
+function startsWithAt(target, candidate, startPos, strictCmp) {
+  const targetLen = target.length;
+  const candidateLen = candidate.length;
+  if (candidateLen - startPos < targetLen) {
+    return false;
+  }
+
+  for (let i = 0; i < targetLen; i += 1) {
+    const targetLetter = target[i];
+    const candidateLetter = candidate[startPos + i];
+
+    const targetLetterUpper = targetLetter.toUpperCase();
+    const candidateLetterUpper = candidateLetter.toUpperCase();
+
+    const targetLetterLenient = accentMap[targetLetterUpper] || targetLetterUpper;
+    const candidateLetterLenient = accentMap[candidateLetterUpper] || candidateLetterUpper;
+
+    if (strictCmp && targetLetter !== candidateLetter) {
+      return false;
+    }
+
+    if (targetLetterLenient !== candidateLetterLenient) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 export function addToKey(answerKey, challenges) {
@@ -62,10 +89,8 @@ export function gradeTranslation(answer, vertices) {
 
   while (stack.length > 0) {
     const [currVertexID, currPos, currVisited] = stack.pop();
-    console.log(`Current vertex: ${currVertexID}`);
 
     if (currVertexID === lastVertexID && currPos >= lastPos) {
-      console.log('Last vertex reached.');
       return true;
     }
 
@@ -75,12 +100,19 @@ export function gradeTranslation(answer, vertices) {
 
         if (!vertex.lenient.trim().length) {
           stack.push([vertex.to, currPos, { ...currVisited, [vertex.to]: null }]);
-        } else if (answerNoSpaces.slice(currPos, currPos + lenientLen) === vertex.lenient) {
+        // eslint-disable-next-line max-len
+        } else if (startsWithAt(vertex.lenient, answerNoSpaces, currPos, true)) {
           stack.push([vertex.to, currPos + lenientLen, { ...currVisited, [vertex.to]: null }]);
         } else if ('orig' in vertex) {
           const origLen = vertex.orig.length;
-          if (caseInsensitiveCmp(vertex.orig, answerNoSpaces.slice(currPos, currPos + origLen))) {
+          const origNoPunctuation = vertex.orig.replace(/[-\s,.?!]/g, '');
+          const origNoPunctuationLen = origNoPunctuation.length;
+          if (startsWithAt(vertex.orig, answerNoSpaces, currPos, false)) {
             stack.push([vertex.to, currPos + origLen, { ...currVisited, [vertex.to]: null }]);
+          // eslint-disable-next-line max-len
+          } else if (startsWithAt(origNoPunctuation, answerNoSpaces, currPos, false)) {
+            // eslint-disable-next-line max-len
+            stack.push([vertex.to, currPos + origNoPunctuationLen, { ...currVisited, [vertex.to]: null }]);
           }
         }
       }
