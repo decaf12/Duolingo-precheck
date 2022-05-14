51e10ff91f2297c6eb5ea5dffa2522950628784b
decaf <siyang.he.uw@gmail.com>
2022-05-14 09:53:20 -0400

    checkpoint

--- testing/testing.js
+++ testing/testing.js
@@ -666,26 +666,23 @@ class marking {
       // const a = new (o()) (((e, t) =>e.weight - t.weight || e.tieBreaker - t.tieBreaker));
       const heap = new heap((e, t) =>e.weight - t.weight || e.tieBreaker - t.tieBreaker);
       heap.push({
-        path: [
-        ],
+        path: [],
         state: currState,
         tieBreaker: 0,
         weight: 0
       });
-      const l = {
-      };
+      const l = {};
 
       // Dijkstra?
       for (; heap.size(); ) {
-        const e = r.Fp(heap.toArray().map((({
-          tieBreaker: e
-        }) =>e))) + 1,
+        const currTieBreaker = r.Fp(heap.toArray().map((({tieBreaker: e}) =>e))) + 1,
         {
           path: currPath,
           state: currState,
           weight: currWeight
         }
         = heap.pop();
+
         if (!l[`${ currState }`]) {
           if (l[`${ currState }`] = true, currWeight > 1) {
             return [null, null];
@@ -697,22 +694,16 @@ class marking {
 
           // Run iteredges and get a list of possible next vertices.
           iterEdgesFunction(currState).forEach((([vertex, nextState], newTieBreaker) =>{
-            const l = edgeWeightFunction(vertex);
-            if (l < 0) {
+            const edgeWeight = edgeWeightFunction(vertex);
+            if (edgeWeight < 0) {
               throw Error('Negative weight edge in solution DAG');
             }
 
             heap.push({
-              path: [
-                ...currPath,
-                [
-                  currState,
-                  vertex
-                ]
-              ],
+              path: [...currPath, [currState, vertex]],
               state: nextState,
-              tieBreaker: e + newTieBreaker,
-              weight: currWeight + l
+              tieBreaker: currTieBreaker + newTieBreaker,
+              weight: currWeight + edgeWeight
             })
           }))
         }
@@ -732,9 +723,8 @@ class marking {
       gradingMetadata: E
     };
 
-    const S = finalWeight <= 0.5,
-    T = {
-    };
+    const weightUnderThreshold = finalWeight <= 0.5,
+    T = {};
     let k = '';
 
     finalPath.forEach((([{
@@ -762,19 +752,17 @@ class marking {
     return {
       blameType: R,
       closestSolution: k,
-      correct: S,
+      correct: weightUnderThreshold,
       highlights: C
     }
   };
 
-  I = e=>{
-    const t = e.filter((([{
-    },
-    e]) =>e.lenient.trim().length > 0)).map((([{
-    },
-    e]) =>e.metadata)),
+  I = e => {
+    const t = e.filter((([{},e]) => e.lenient.trim().length > 0)).map((([{},e]) =>e.metadata)),
     n = t.filter((e=>void 0 !== e));
-    if (0 !== n.length && n.length === t.length) return n[0].filter((e=>n.every((t=>t.some((t=>r.Xy(e, t)))))))
+    if (0 !== n.length && n.length === t.length) {
+      return n[0].filter((e => n.every((t=>t.some((t=>r.Xy(e, t)))))))
+    }
   };
 
   const accentcharactermap = JSON.parse('{"À":"A","Á":"A","Â":"A","Ã":"A","Ä":"A","Å":"A","Ç":"C","È":"E","É":"E","Ê":"E","Ë":"E","Ì":"I","Í":"I","Î":"I","Ï":"I","Ð":"D","Ñ":"N","Ò":"O","Ó":"O","Ô":"O","Õ":"O","Ö":"O","×":"x","Ø":"O","Ù":"U","Ú":"U","Û":"U","Ü":"U","Ý":"Y","à":"a","á":"a","â":"a","ã":"a","ä":"a","å":"a","ç":"c","è":"e","é":"e","ê":"e","ë":"e","ì":"i","í":"i","î":"i","ï":"i","ð":"d","ñ":"n","ò":"o","ó":"o","ô":"o","õ":"o","ö":"o","÷":"/","ø":"o","ù":"u","ú":"u","û":"u","ü":"u","ý":"y","ÿ":"y","Ā":"A","ā":"a","Ă":"A","ă":"a","Ą":"A","ą":"a","Ć":"C","ć":"c","Ĉ":"C","ĉ":"c","Ċ":"C","ċ":"c","Č":"C","č":"c","Ď":"D","ď":"d","Đ":"D","đ":"d","Ē":"E","ē":"e","Ĕ":"E","ĕ":"e","Ė":"E","ė":"e","Ę":"E","ę":"e","Ě":"E","ě":"e","Ĝ":"G","ĝ":"g","Ğ":"G","ğ":"g","Ġ":"G","ġ":"g","Ģ":"G","ģ":"g","Ĥ":"H","ĥ":"h","Ħ":"H","ħ":"h","Ĩ":"I","ĩ":"i","Ī":"I","ī":"i","Ĭ":"I","ĭ":"i","Į":"I","į":"i","İ":"I","ı":"i","Ĵ":"J","ĵ":"j","Ķ":"K","ķ":"k","ĸ":"k","Ĺ":"L","ĺ":"l","Ļ":"L","ļ":"l","Ľ":"L","ľ":"l","Ŀ":"L","ŀ":"l","Ł":"L","ł":"l","Ń":"N","ń":"n","Ņ":"N","ņ":"n","Ň":"N","ň":"n","Ō":"O","ō":"o","Ŏ":"O","ŏ":"o","Ő":"O","ő":"o","Ŕ":"R","ŕ":"r","Ŗ":"R","ŗ":"r","Ř":"R","ř":"r","Ś":"S","ś":"s","Ŝ":"S","ŝ":"s","Ş":"S","ş":"s","Š":"S","š":"s","Ţ":"T","ţ":"t","Ť":"T","ť":"t","Ŧ":"T","ŧ":"t","Ũ":"U","ũ":"u","Ū":"U","ū":"u","Ŭ":"U","ŭ":"u","Ů":"U","ů":"u","Ű":"U","ű":"u","Ų":"U","ų":"u","Ŵ":"W","ŵ":"w","Ŷ":"Y","ŷ":"y","Ÿ":"Y","Ź":"Z","ź":"z","Ż":"Z","ż":"z","Ž":"Z","ž":"z","Ơ":"O","ơ":"o","Ư":"U","ư":"u","Ș":"S","ș":"s","Ț":"T","ț":"t","Ά":"Α","Έ":"Ε","Ή":"Η","Ί":"Ι","Ό":"Ο","Ύ":"Υ","Ώ":"Ω","ΐ":"ι","Ϊ":"Ι","Ϋ":"Υ","ά":"α","έ":"ε","ή":"η","ί":"ι","ΰ":"υ","ϊ":"ι","ϋ":"υ","ό":"ο","ύ":"υ","ώ":"ω","Ạ":"A","ạ":"a","Ả":"A","ả":"a","Ấ":"A","ấ":"a","Ầ":"A","ầ":"a","Ẩ":"A","ẩ":"a","Ẫ":"A","ẫ":"a","Ậ":"A","ậ":"a","Ắ":"A","ắ":"a","Ằ":"A","ằ":"a","Ẳ":"A","ẳ":"a","Ẵ":"A","ẵ":"a","Ặ":"A","ặ":"a","Ẹ":"E","ẹ":"e","Ẻ":"E","ẻ":"e","Ẽ":"E","ẽ":"e","Ế":"E","ế":"e","Ề":"E","ề":"e","Ể":"E","ể":"e","Ễ":"E","ễ":"e","Ệ":"E","ệ":"e","Ỉ":"I","ỉ":"i","Ị":"I","ị":"i","Ọ":"O","ọ":"o","Ỏ":"O","ỏ":"o","Ố":"O","ố":"o","Ồ":"O","ồ":"o","Ổ":"O","ổ":"o","Ỗ":"O","ỗ":"o","Ộ":"O","ộ":"o","Ớ":"O","ớ":"o","Ờ":"O","ờ":"o","Ở":"O","ở":"o","Ỡ":"O","ỡ":"o","Ợ":"O","ợ":"o","Ụ":"U","ụ":"u","Ủ":"U","ủ":"u","Ứ":"U","ứ":"u","Ừ":"U","ừ":"u","Ử":"U","ử":"u","Ữ":"U","ữ":"u","Ự":"U","ự":"u","Ỳ":"Y","ỳ":"y","Ỵ":"Y","ỵ":"y","Ỷ":"Y","ỷ":"y","Ỹ":"Y","ỹ":"y"}');;
