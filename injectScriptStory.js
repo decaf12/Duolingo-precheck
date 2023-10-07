bcf963dcdfaec15645c3cbae73267536ce846e81
Siyang He <siyang.he.uw@gmail.com>
2023-10-06 23:54:31 -0400

    fix module issues

--- injectScriptStory.js
+++ injectScriptStory.js
@@ -1,5 +1,6 @@
 const storyScript = document.createElement('script');
 storyScript.src = chrome.runtime.getURL('addListenersStory.js');
+storyScript.setAttribute('type', 'module');
 // Don't use an arrow function here, otherwise 'this' would have no meaning.
 storyScript.onload = function () { this.remove(); };
 (document.head || document.documentElement).appendChild(storyScript);
