93507658813ffd38941423f40a69fe325c4cc506
decaf12 <siyang.he.uw@gmail.com>
2024-04-30 11:50:46 -0400

    story point to phrase fix

--- src/stories/challengeTypeConstants.js
+++ src/stories/challengeTypeConstants.js
@@ -3,8 +3,8 @@ export const STORY_ANSWERKEY = '[data-test="stories-element"]';
 export const STORY_CHECKBOX = 'button[data-test="stories-choice"]';
 export const STORY_CHOICE = '[class="_34A44"]';
 
-export const STORY_TOKENS = 'button[data-test$="challenge-tap-token"]';
-export const STORY_TOKEN_SELECTED = '[class="_33Jbm _1N-oo _36Vd3 _16r-S notranslate _356DE _25kHK _3U5_i"]';
+export const STORY_TOKENS = 'button[data-test*="challenge-tap-token"]';
+export const STORY_TOKEN_SELECTED = `${STORY_TOKENS}[aria-disabled="true"]`;
 export const STORY_TOKEN_BANK = '[data-test="word-bank"]';
 
 export const MATCH_BUTTON_GROUP = 'div[style^="flex-direction"]';
