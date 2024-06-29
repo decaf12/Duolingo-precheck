9086662ddce256d64402006e8918f9bc97078d11
Siyang He <siyang.he.uw@gmail.com>
2024-06-28 21:19:13 -0400

    cleanup

--- src/stories/challengeTypeConstants.js
+++ src/stories/challengeTypeConstants.js
@@ -1,23 +1,11 @@
 export const STORY_PARENT = '[data-test="stories-element"]';
-export const STORY_ANSWERKEY = '[data-test="stories-element"]';
 export const STORY_CHECKBOX = 'button[data-test="stories-choice"]';
 export const STORY_CHOICE = '[class="_34A44"]';
-
 export const STORY_TOKENS = 'button[data-test*="challenge-tap-token"]';
 export const STORY_TOKEN_SELECTED = `${STORY_TOKENS}[aria-disabled="true"]`;
 export const STORY_TOKEN_BANK = '[data-test="word-bank"]';
-
 export const MATCH_BUTTON_GROUP = 'div[style^="flex-direction"]';
-export const MATCH_BUTTONS = '[class="_3mPzB"]';
 export const MATCH_BUTTON_SELECTED = 'button[class*="_3Nuad"]';
 export const MATCH_BUTTON_TEXT = '[data-test="challenge-tap-token-text"]';
-export const MATCH_BUTTON_PARENT = '[class="_1gBvk"]';
-export const MATCH_BUTTON_LABEL = '[class="_2IiaT"]';
-export const MATCH_BUTTON_LABEL_TOKENS = '[class="z4ACb _1umuN"]';
 export const MATCH_BUTTON_NUMBER = 'span[class^="_3"]';
-export const MATCH_BUTTON_NUMBER_UNSELECTED = '[class="_2eLV9 _1fa4a _2rSFV"]';
-export const MATCH_BUTTON_NUMBER_SELECTED = '[class="_3Nzby _1fa4a _2rSFV"]';
-export const MATCH_BUTTON_NUMBER_GREYED = '[class="_33WH4 _1fa4a _2rSFV"]';
-
 export const POINT_TO_PHRASE_BUTTON_BANK = 'div[dir]';
-export const POINT_TO_PHRASE_BUTTON = '[data-test="challenge-tap-token-text"]';
