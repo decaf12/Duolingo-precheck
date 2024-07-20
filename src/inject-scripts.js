0bdc393b42665972df60091edb3cb8fffda0cc1c
Siyang He <siyang.he.uw@gmail.com>
2024-07-20 08:20:43 -0400

    fix

--- src/inject-scripts.js
+++ src/inject-scripts.js
@@ -3,11 +3,18 @@ function injectScript(src) {
   const script = document.createElement('script');
   script.setAttribute('type', 'module');
   script.src = browser.runtime.getURL(src);
-  script.onload = () => script.remove();
+  script.onload = () => {
+    debugger;
+    script.remove();
+  };
+  script.onerror = (error) => {
+    debugger;
+  };
   (document.head || document.documentElement).appendChild(script);
 }
 
+debugger;
 injectScript('src/lessons/add-listeners-lesson.js');
 injectScript('src/lessons/add-listeners-match.js');
 injectScript('src/stories/add-match-keyboard-listener-story.js');
-injectScript('src/stories/add-selectable-keyboard-listener-story.js');
+injectScript('src/stories/add-selectable-listener-story.js');
--- src/lessons/add-listeners-lesson.js
+++ src/lessons/add-listeners-lesson.js
@@ -6,10 +6,12 @@ import { checkMatch } from './utils/check-match';
 import { checkListenMatch } from './utils/check-listen-match';
 import { getButton } from './utils/get-button';
 
+debugger;
 document.addEventListener(
   'keydown',
   (e) => {
     if (e.key === 'Enter') {
+      debugger;
       const submissionButton = document.querySelector(constants.SUBMISSION_BUTTON_LESSON);
 
       if (submissionButton && !checkSubmission(submissionButton)) {
@@ -17,6 +19,7 @@ document.addEventListener(
         e.stopImmediatePropagation();
       }
     } else if (/^\d$/.test(e.key)) {
+      debugger;
       const challengeData = getChallengeDataLesson();
       if (challengeData.type !== 'match' && challengeData.type !== 'listenMatch') {
         return;
